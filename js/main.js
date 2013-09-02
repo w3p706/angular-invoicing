632cbcad89b938a90437dfbcf2a1971f27ee96f9
Fabian Heusser <fabian.heusser@gmail.com>
2013-09-02 22:51:45 +0200

    Customizing

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ function InvoiceController($scope) {
   $scope.printMode = false;
 
   var sample_invoice = {
-            tax: 13.00, 
-            invoice_number: 10,
+            tax: 13.00,
+    invoice_number: 10,
+    invoice_date: new Date(),
             customer_info:  {name: "Mr. John Doe", web_link: "John Doe Designs Inc.", address1: "1 Infinite Loop", address2: "Cupertino, California, US", postal: "90210"},
-            company_info:  {name: "Metaware Labs", web_link: "www.metawarelabs.com", address1: "123 Yonge Street", address2: "Toronto, ON, Canada", postal: "M5S 1B6"},
+            company_info:  {name: "Hans Muster", web_link: "Testdrive 27", address1: "6000 Testhausen", address2: "test@beispiel.ch", postal: ""},
               items:[ {qty:10, description:'Gadget', cost:9.95}]};
 
   if(localStorage["invoice"] == "" || localStorage["invoice"] == null){
